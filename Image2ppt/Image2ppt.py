9b1d780b81b28b005882914df4762f62214c55d4
Tofu <osawa.shuto@gmail.com>
2022-04-03 01:59:50 +0900

    moved some variables from function to constructor

--- Image2ppt/Image2ppt.py
+++ Image2ppt/Image2ppt.py
@@ -34,29 +34,26 @@ class AppendPPT():
 
 class AppendSlide():
 
+    def __init__(self):
+        self.column = 4
+        self.row = 2
+        self.ppt_width = 13.333
+        self.ppt_height = 7.5
+        self.img_iter = 8
+
     def append_images_in_ppt(self,img_count,input_path,img):
         #Generate ppt and specify slide types
         prs = Presentation()
         blank_slide =  prs.slide_layouts[6]
-
-        ppt_width = 13.333
-        ppt_height = 7.5
-
-        column = 4
-        row = 2
-
-        prs.slide_width = Inches(ppt_width)
-        prs.slide_height = Inches(ppt_height)
-        
-        
-        img_iter = 8
+        prs.slide_width = Inches(self.ppt_width)
+        prs.slide_height = Inches(self.ppt_height)      
 
         for i in range(img_count):
-            if(i%img_iter == 0):
+            if(i%self.img_iter == 0):
                 image_slide = prs.slides.add_slide(blank_slide)
-            arg1 = Inches((i%column)*(ppt_width/column))
-            arg2 = Inches((i%img_iter//column)*ppt_height/row)
-            image_slide.shapes.add_picture(input_path+'/'+img[i], arg1, arg2, height=Inches(ppt_width/column))
+            arg1 = Inches((i%self.column)*(self.ppt_width/self.column))
+            arg2 = Inches((i%self.img_iter//self.column)*self.ppt_height/self.row)
+            image_slide.shapes.add_picture(input_path+'/'+img[i], arg1, arg2, height=Inches(self.ppt_width/self.column))
 
         return prs
     
