c1f4f34f1b356cd878f38238a66cf15cf12bb191
Tofu <osawa.shuto@gmail.com>
2022-04-03 00:17:59 +0900

    separated image loop in a different function

--- Image2ppt/Image2ppt.py
+++ Image2ppt/Image2ppt.py
@@ -8,13 +8,19 @@ from tkinter import filedialog
 def main():
     tkinter.Tk().withdraw()
     #Input path then output path
-    path = filedialog.askdirectory()
-    print(path)
-    img = os.listdir(path)
+    input_path = filedialog.askdirectory()
+    print(input_path)
+    img = os.listdir(input_path)
     img_count = len(img)
+
     print(img_count)
+
     output_path = filedialog.askdirectory()
 
+    prs = append_images_in_ppt(img_count,input_path,img)
+    prs.save(output_path+'/test.pptx')
+
+def append_images_in_ppt(img_count,input_path,img):
     #Generate ppt and specify slide types
     prs = Presentation()
     blank_slide =  prs.slide_layouts[6]
@@ -22,13 +28,13 @@ def main():
     #Specify dimensions
     prs.slide_width = Inches(13.333)
     prs.slide_height = Inches(7.5)
+    
     height = Inches(3.33)
     left = top = Inches(0)
     left2 = Inches(3.33)
     right = Inches(6.67)
     right2 = Inches(10)
     bottom = Inches(4.17)
-
 
     for i in range(img_count):
         if(i%8 == 0):
@@ -48,10 +54,11 @@ def main():
         elif(i%8 > 3):
             arg2 = bottom
 
-        image_slide.shapes.add_picture(path+'/'+img[i], arg1, arg2, height=height)
+        image_slide.shapes.add_picture(input_path+'/'+img[i], arg1, arg2, height=height)
 
+    return prs
     #specify savename
-    prs.save(output_path+'/test.pptx')
+    
 
 if __name__ == "__main__":
     main()
