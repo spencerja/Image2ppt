866421e666d198880c311906fc335e0ce1fd410e
Tofu <osawa.shuto@gmail.com>
2022-04-03 01:54:35 +0900

    removed some magic numbers

--- Image2ppt/Image2ppt.py
+++ Image2ppt/Image2ppt.py
@@ -39,39 +39,26 @@ class AppendSlide():
         prs = Presentation()
         blank_slide =  prs.slide_layouts[6]
 
-        #Specify dimensions
-        prs.slide_width = Inches(13.333)
-        prs.slide_height = Inches(7.5)
-    
-        height = Inches(3.33)
-        left = top = Inches(0)
-        left2 = Inches(3.33)
-        right = Inches(6.67)
-        right2 = Inches(10)
-        bottom = Inches(4.17)
+        ppt_width = 13.333
+        ppt_height = 7.5
+
+        column = 4
+        row = 2
+
+        prs.slide_width = Inches(ppt_width)
+        prs.slide_height = Inches(ppt_height)
+        
+        
+        img_iter = 8
 
         for i in range(img_count):
-            if(i%8 == 0):
+            if(i%img_iter == 0):
                 image_slide = prs.slides.add_slide(blank_slide)
-        
-            if(i%4 ==0):
-                arg1 = left
-            elif(i%4 ==1):
-                arg1 = left2
-            elif(i%4 ==2):
-                arg1 = right
-            elif(i%4 ==3):
-                arg1 = right2
-
-            if(i%8 < 4):
-                arg2 = top
-            elif(i%8 > 3):
-                arg2 = bottom
-
-            image_slide.shapes.add_picture(input_path+'/'+img[i], arg1, arg2, height=height)
+            arg1 = Inches((i%column)*(ppt_width/column))
+            arg2 = Inches((i%img_iter//column)*ppt_height/row)
+            image_slide.shapes.add_picture(input_path+'/'+img[i], arg1, arg2, height=Inches(ppt_width/column))
 
         return prs
-    
     
 
 if __name__ == "__main__":
