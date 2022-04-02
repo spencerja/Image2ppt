6658c928f519a6ef9f426c35fc67e7ff4faa65ff
Tofu <osawa.shuto@gmail.com>
2022-04-03 02:16:53 +0900

    refactored everything

--- Image2ppt/Image2ppt.py
+++ Image2ppt/Image2ppt.py
@@ -13,47 +13,62 @@ def main():
 
 class AppendPPT():
 
+
+    def get_path(self):
+        selected_path = filedialog.askdirectory()
+        return selected_path
+
     def whole_process(self):
         tkinter.Tk().withdraw()
-        #Input path then output path
-        input_path = filedialog.askdirectory()
-        print(input_path)
-        img = os.listdir(input_path)
-        img_count = len(img)
 
-        print(img_count)
+        input_path = self.get_path()
+        
 
-        output_path = filedialog.askdirectory()
+        output_path = self.get_path()
 
-        append_slide = AppendSlide()
+        append_slide = AppendSlide(input_path)
 
-        prs = append_slide.append_images_in_ppt(img_count,input_path,img)
+        prs = append_slide.append_images_in_ppt()
 
-        #specify savename
         prs.save(output_path+'/test.pptx')
 
 class AppendSlide():
 
-    def __init__(self):
+    #initial value loading
+    def __init__(self,input_path):
         self.column = 4
         self.row = 2
         self.ppt_width = 13.333
         self.ppt_height = 7.5
         self.img_iter = 8
+        self.img = self.get_images(input_path)
+        self.img_count = len(self.img)
+        self.input_path = input_path
+        
 
-    def append_images_in_ppt(self,img_count,input_path,img):
-        #Generate ppt and specify slide types
+    def get_images(self,input_path):
+        img = os.listdir(input_path)
+        return img
+
+    #generate ppt and add images to the ppt
+    def append_images_in_ppt(self):        
         prs = Presentation()
-        blank_slide =  prs.slide_layouts[6]
         prs.slide_width = Inches(self.ppt_width)
         prs.slide_height = Inches(self.ppt_height)      
+        prs = self.append_images(prs)
+        return prs
 
-        for i in range(img_count):
-            if(i%self.img_iter == 0):
+    def append_images(self,prs):
+        blank_slide =  prs.slide_layouts[6]
+
+        for i in range(self.img_count):
+            if(i%self.img_iter == 0):                
                 image_slide = prs.slides.add_slide(blank_slide)
+
             arg1 = Inches((i%self.column)*(self.ppt_width/self.column))
             arg2 = Inches((i%self.img_iter//self.column)*self.ppt_height/self.row)
-            image_slide.shapes.add_picture(input_path+'/'+img[i], arg1, arg2, height=Inches(self.ppt_width/self.column))
+
+            image_slide.shapes.add_picture(self.input_path+'/'+self.img[i], arg1, arg2, height=Inches(self.ppt_width/self.column))
 
         return prs
     
