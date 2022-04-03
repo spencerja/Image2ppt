c6bb1c031f41a6e1ab82a927683b205d2955fcd1
Tofu <osawa.shuto@gmail.com>
2022-04-03 20:47:57 +0900

    components are separated in different class

--- Image2ppt/Image2ppt.py
+++ Image2ppt/Image2ppt.py
@@ -8,76 +8,83 @@ from tkinter import ttk
 
 # Utilizes python-pptx: https://python-pptx.readthedocs.io/
 
+
 def main():
 
     create_gui = CreateGUI()
     create_gui.construct_form()
 
-class CreateGUI:
-    def __init__(self):
-        self.root = Tk()
-        self.root.minsize(width=500, height=300)
-        self.root.title("Image2ppt")
-        self.frame = None
-        self.input_path_label = None
-        self.ppt_name_textbox = None
-        self.path_list = [r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Input",
-                          r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Output"]
 
-    def construct_form(self):
-        self.create_widget()
-        self.input_path_label = self.create_label(self.path_list[0], 0, 0)
-        input_path_button = self.create_button("Input path",0,1)
-        input_path_button.bind("<ButtonPress>", lambda event: self.get_path(event, self.input_path_label))
+class Components:
 
-        self.output_path_label = self.create_label(self.path_list[1], 1, 0)
-        output_path_button = self.create_button("Output path", 1, 1)
-        output_path_button.bind("<ButtonPress>", lambda event: self.get_path(event, self.output_path_label))
-
-        self.ppt_name_textbox = self.create_textbox("test", 2, 0)
-
-        start_process_button = self.create_button("Start",2,1)
-        start_process_button.bind("<ButtonPress>", lambda event: self.whole_process(event))
-
-        self.start_widget()
-
-    def create_widget(self):
-        self.frame = ttk.Frame(self.root, padding=40)
-        self.frame.grid()
-
-    def update_path_list(self):
-        self.path_list = [self.input_path_label.cget("text"), self.output_path_label.cget("text")]
-
-    def create_textbox(self,text,row,column):
-        txt = ttk.Entry(self.frame,width=40)
+    def create_textbox(self,frame,text,row,column):
+        txt = ttk.Entry(frame,width=40)
         txt.insert(tkinter.END,text)
         txt.grid(row=row,column=column)
         return txt
 
-    def create_label(self,text,row,column):
+    def create_label(self,frame,text,row,column):
         label1 = ttk.Label(
-            self.frame,
+            frame,
             text=text,
             padding=(5, 10))
         label1.grid(row=row, column=column)
         return label1
 
-    def create_button(self,text,row,column):
+    def create_button(self,frame, text, row, column):
         button1 = ttk.Button(
-            self.frame,
+            frame,
             text=text,
             )
         button1.grid(row=row, column=column)
         return button1
 
+    def create_frame(self,root):
+        frame = ttk.Frame(root, padding=40)
+        frame.grid()
+        return frame
 
-    def get_path(self, event,arg):
+class CreateGUI:
+    def __init__(self):
+
+        self.input_path_label = "initial path"
+        self.output_path_label = "initial path"
+        self.ppt_name_textbox = "test"
+        self.path_list = [r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Input",
+                          r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Output"]
+
+    def construct_form(self):
+        components = Components()
+        root = Tk()
+        root.minsize(width=500, height=300)
+        root.title("Image2ppt")
+        frame = components.create_frame(root)
+
+        self.input_path_label = components.create_label(frame, self.path_list[0], 0, 0)
+
+        input_path_button = components.create_button(frame, "Input path", 0, 1)
+        input_path_button.bind("<ButtonPress>", lambda event: self.get_path(event, self.input_path_label))
+
+        self.output_path_label = components.create_label(frame, self.path_list[1], 1, 0)
+
+        output_path_button = components.create_button(frame,"Output path", 1, 1)
+        output_path_button.bind("<ButtonPress>", lambda event: self.get_path(event, self.output_path_label))
+
+        self.ppt_name_textbox = components.create_textbox(frame, "test", 2, 0)
+
+        start_process_button = components.create_button(frame, "Start", 2, 1)
+        start_process_button.bind("<ButtonPress>", lambda event: self.ppt_generation_process(event))
+
+        self.start_gui(root)
+
+
+    def get_path(self, event, arg):
         selected_path = filedialog.askdirectory()
         if not selected_path=="":
             arg.configure(text=selected_path)
 
-    def whole_process(self,event):
-        tkinter.Tk().withdraw()
+    def ppt_generation_process(self, event):
+        #tkinter.Tk().withdraw()
         path_list = [self.input_path_label.cget("text"), self.output_path_label.cget("text")]
         input_path = path_list[0]
         output_path = path_list[1]
@@ -86,8 +93,8 @@ class CreateGUI:
         prs.save(output_path + '/' + self.ppt_name_textbox.get() + '.pptx')
         os.startfile(output_path + '/' + self.ppt_name_textbox.get() + '.pptx')
 
-    def start_widget(self):
-        self.root.mainloop()
+    def start_gui(self,root):
+        root.mainloop()
 
 
 #this is not used in production
@@ -104,6 +111,9 @@ class AppendSlide:
 
         self.ppt_width = 13.333
         self.ppt_height = 7.5
+
+        self.img_width = self.ppt_width/self.column
+        self.img_height = self.ppt_height/self.row
 
         self.img_iter = self.column * self.row
 
@@ -134,11 +144,15 @@ class AppendSlide:
             horizontal = Inches((i % self.column) * (self.ppt_width / self.column))
             vertical = Inches((i % self.img_iter // self.column) * self.ppt_height / self.row)
 
-            image_slide.shapes.add_picture(self.input_path + '/' + self.img_list[i], horizontal, vertical,
-                                           height=Inches(self.ppt_width / self.column))
+
+            if self.img_width < self.img_height:
+                image_slide.shapes.add_picture(self.input_path + '/' + self.img_list[i], horizontal, vertical,
+                                               height=Inches(self.ppt_width / self.column))
+            else:
+                image_slide.shapes.add_picture(self.input_path + '/' + self.img_list[i], horizontal, vertical,
+                                            width=Inches(self.ppt_height / self.row))
 
         return prs
-
 
 
 if __name__ == "__main__":
