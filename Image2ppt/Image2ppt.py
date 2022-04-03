29ba9fbc1ba57e9fd661a6a9883170d67d8c4f07
Tofu <osawa.shuto@gmail.com>
2022-04-03 18:37:32 +0900

    renamed variables

--- Image2ppt/Image2ppt.py
+++ Image2ppt/Image2ppt.py
@@ -19,25 +19,25 @@ class CreateGUI:
         self.root.minsize(width=500, height=300)
         self.root.title("Image2ppt")
         self.frame = None
-        self.label1 = None
-        self.textbox = None
-        self.path_list = [r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Input", r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Output"]
+        self.input_path_label = None
+        self.ppt_name_textbox = None
+        self.path_list = [r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Input",
+                          r"C:\Users\Fridge\Documents\PYGit\Image2ppt\Image2ppt\Output"]
 
     def construct_form(self):
         self.create_widget()
-        self.label1 = self.create_label(self.path_list[0],0,0)
-        button1 = self.create_button("Input path",0,1)
-        button1.bind("<ButtonPress>",lambda event: self.get_path(event,self.label1))
+        self.input_path_label = self.create_label(self.path_list[0], 0, 0)
+        input_path_button = self.create_button("Input path",0,1)
+        input_path_button.bind("<ButtonPress>", lambda event: self.get_path(event, self.input_path_label))
 
-        self.label2 = self.create_label(self.path_list[1],1,0)
-        button2 = self.create_button("Output path", 1, 1)
-        button2.bind("<ButtonPress>", lambda event: self.get_path(event,self.label2))
+        self.output_path_label = self.create_label(self.path_list[1], 1, 0)
+        output_path_button = self.create_button("Output path", 1, 1)
+        output_path_button.bind("<ButtonPress>", lambda event: self.get_path(event, self.output_path_label))
 
-        self.textbox = self.create_textbox("test", 2, 0)
+        self.ppt_name_textbox = self.create_textbox("test", 2, 0)
 
-        button3 = self.create_button("Start",2,1)
-        button3.bind("<ButtonPress>", lambda event: self.whole_process(event))
-
+        start_process_button = self.create_button("Start",2,1)
+        start_process_button.bind("<ButtonPress>", lambda event: self.whole_process(event))
 
         self.start_widget()
 
@@ -46,7 +46,7 @@ class CreateGUI:
         self.frame.grid()
 
     def update_path_list(self):
-        self.path_list = [self.label1.cget("text"), self.label2.cget("text")]
+        self.path_list = [self.input_path_label.cget("text"), self.output_path_label.cget("text")]
 
     def create_textbox(self,text,row,column):
         txt = ttk.Entry(self.frame,width=40)
@@ -78,43 +78,23 @@ class CreateGUI:
 
     def whole_process(self,event):
         tkinter.Tk().withdraw()
-        append_ppt = AppendPPT()
-        path_list = [self.label1.cget("text"), self.label2.cget("text")]
+        path_list = [self.input_path_label.cget("text"), self.output_path_label.cget("text")]
         input_path = path_list[0]
         output_path = path_list[1]
-
         append_slide = AppendSlide(input_path)
-
         prs = append_slide.append_images_in_ppt()
-        prs.save(output_path + '/'+ self.textbox.get()+'.pptx')
-        os.startfile(output_path + '/'+ self.textbox.get()+'.pptx')
+        prs.save(output_path + '/' + self.ppt_name_textbox.get() + '.pptx')
+        os.startfile(output_path + '/' + self.ppt_name_textbox.get() + '.pptx')
 
     def start_widget(self):
         self.root.mainloop()
-
-
-class AppendPPT:
-
-    def get_path(self):
-        selected_path = filedialog.askdirectory()
-        return selected_path
-
-    def whole_process(self):
-        tkinter.Tk().withdraw()
-
-        input_path = self.get_path()
-        output_path = self.get_path()
-
-        append_slide = AppendSlide(input_path)
-
-        prs = append_slide.append_images_in_ppt()
-        prs.save(output_path + '/test.pptx')
 
 
 #this is not used in production
 class AddTest:
     def add_test(self,a,b):
         return a+b
+
 
 class AppendSlide:
     # initial value loading
@@ -127,14 +107,14 @@ class AppendSlide:
 
         self.img_iter = self.column * self.row
 
-        self.img = self.get_images(input_path)
-        self.img_count = len(self.img)
+        self.img_list = self.get_images(input_path)
+        self.img_count = len(self.img_list)
 
         self.input_path = input_path
 
     def get_images(self, input_path):
-        img = os.listdir(input_path)
-        return img
+        img_list = os.listdir(input_path)
+        return img_list
 
     # generate ppt and add images to the ppt
     def append_images_in_ppt(self):
@@ -151,10 +131,10 @@ class AppendSlide:
             if i % self.img_iter == 0:
                 image_slide = prs.slides.add_slide(blank_slide)
 
-            arg1 = Inches((i % self.column) * (self.ppt_width / self.column))
-            arg2 = Inches((i % self.img_iter // self.column) * self.ppt_height / self.row)
+            horizontal = Inches((i % self.column) * (self.ppt_width / self.column))
+            vertical = Inches((i % self.img_iter // self.column) * self.ppt_height / self.row)
 
-            image_slide.shapes.add_picture(self.input_path + '/' + self.img[i], arg1, arg2,
+            image_slide.shapes.add_picture(self.input_path + '/' + self.img_list[i], horizontal, vertical,
                                            height=Inches(self.ppt_width / self.column))
 
         return prs
